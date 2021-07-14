db82c7ca66dbe296d36d916ad00d8be28f0724eb
Amit <v99amit@gmail.com>
2021-07-14 21:47:47 +0530

    Initial commit

---
+++ Pacman_Game/src/pacman/game/Pacman.java
@@ -0,0 +1,42 @@
+package pacman.game;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame{
+	
+	public Pacman() {
+		
+		initUI();
+	
+	}
+	
+	private void initUI() {
+		add(new Board());
+		
+		setTitle("Pacman");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(380,420);
+		setLocationRelativeTo(null);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			
+			Pacman ex = new Pacman();
+			ex.setVisible(true);
+			
+			String filepath = "pacmanMusic.wav";
+			Board musicObject = new Board();
+			musicObject.playMusic(filepath);
+		
+
+	});
+		
+		
+	}
+
+}
